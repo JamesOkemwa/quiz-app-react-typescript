24a20f5959d9e2a04e0035654991ae60ef68435d
lejames <okejames55@gmail.com>
2022-08-16 15:49:21 +0300

    Question card props

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const QuestionCard = () => {
+type QuestionProps = {
+  question: string
+  answers: string[]
+  callback: any
+  userAnswer: any
+  questionNr: number
+  totalQuestions: number
+}
+
+const QuestionCard = ({question, answers, callback, userAnswer, questionNr, totalQuestions}: QuestionProps) => {
   return (
-    <div>QuestionCard</div>
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question}}/>
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer}}/>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
